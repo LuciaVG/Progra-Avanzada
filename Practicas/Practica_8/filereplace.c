4059107a82aa1a7f11520ca3ca8932d88dd7cf48
LuciaVG <bvluly@gmail.com>
2018-04-09 19:38:21 -0500

    Practica 9 parte 1

--- Practicas/Practica_8/filereplace.c
+++ Practicas/Practica_8/filereplace.c
@@ -1,42 +1,58 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <strings.h>
-#include <string.h>
 
-#define printDebug(...) do{\
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define printDebug(...) \
+do \
+{ \
   char *isDebug; \
   isDebug = getenv("DEBUG"); \
-  if(isDebug != NULL && !strcasecmp(isDebug, "y")){ \
+  if (isDebug != NULL && !strcasecmp(isDebug, "y")) \
+  { \
     fprintf(stderr, __VA_ARGS__); \
   } \
-}while(0)
+} while (0)
 
 int main(){
-    char *fileName = "input.txt";
-    FILE *input;
-    input = fopen(fileName, "r");
-    char *search = "mundo";
-    char *replace = "MUNDOOOOO";
-    char word[255];
-    int character;
-    long searchPos;
-    while (1){
-      fscanf(input, "%s", word);
-      if (feof(input)){
-        break;
-      }
-      if(strcmp(word, search) == 0){
-        printDebug("Found %s\n", word);
-        fseek(input, -1*strlen(search),SEEK_CUR);
-        searchPos = ftell(input);
-        printDebug("searchPos = %ld\n", searchPos);
-        rewind(input);
-        while(ftell(input) != searchPos){
-          character = fgetc(input);
-          fputc(character, stdin);
-        }
-      }
-    }
-    return 0;
+  char *fileName = "input.txt";
+  FILE *input;
+  char *search = "mundo";
+  char *replace = "MUNDOOOOO";
+  char word[255];
+  long searchPos = 0;
+  int character;
+  input = fopen(fileName, "r");
 
+  while (1){
+    fscanf(input,"%s",word);
+    if(feof(input)){
+      break;
+    }
+
+    if(strcmp(word,search) == 0) {
+      printDebug("Found %s\n", word);
+      fseek(input,-1*strlen(search),SEEK_CUR);
+      searchPos = ftell(input);
+      printDebug("searchPos = %ld\n", searchPos);
+      rewind(input);
+      while(ftell(input) != searchPos) {
+        character = fgetc(input);
+        fputc(character,stdout);
+      }
+
+      fputs(replace,stdout);
+      fseek(input,strlen(word),SEEK_CUR);
+
+      while(1){
+        character = fgetc(input);
+        if(feof(input)) {
+          break;
+        }
+        fputc(character,stdout);
+      }
+      break;
+    }
+  }
+  return 0;
 }
