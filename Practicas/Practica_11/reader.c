15d858fbc7075dba3fdcb79e18d1c28e3c3f6ddb
LuciaVG <bvluly@gmail.com>
2018-04-16 20:29:50 -0500

    Practica 11

---
+++ Practicas/Practica_11/reader.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <string.h>
+#include "student.h"
+
+
+int main(int argc, char const *argv[]) {
+  char *fileName = "student.dat";
+  int fd = open(fileName, O_RDWR);
+  int id;
+  char name[20];
+  Student *pAvanzada = (Student *)mmap(NULL, 10*sizeof(Student),PROT_READ | PROT_WRITE, MAP_FILE | MAP_SHARED, fd, 0);
+  close(fd);
+  for(int i = 0; i<4; i++){
+    printf("Give an ID\n");
+    scanf("%d", &id);
+    //while(getchar() != '\n');
+    printf("Student[%d] = %s\n", id, pAvanzada[id].firstName);
+  }
+
+  munmap(pAvanzada, 10*sizeof(Student));
+  return 0;
+}
---
+++ Practicas/Practica_11/student.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+typedef struct s{
+  char firstName[20];
+  char lastName[20];
+  short Id;
+  char semester[3];
+  char free;
+} Student;
